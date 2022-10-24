098b43879e2db0d8c6cde0d9b2a03443ae90ee7d
Koshyar-r <belfi7777@gmail.com>
2022-10-24 14:06:46 -0700

    initial commit

---
+++ assets/js/script.js
@@ -0,0 +1,74 @@
+const navMenu = document.getElementById('nav-menu'),
+      navToggle = document.getElementById('nav-toggle'),
+      navClose = document.getElementById('nav-close')
+
+
+if(navToggle){
+    navToggle.addEventListener('click', () =>{
+        navMenu.classList.add('show-menu')
+    })
+}
+
+if(navClose){
+    navClose.addEventListener('click', () =>{
+        navMenu.classList.remove('show-menu')
+    })
+}
+
+const navLink = document.querySelectorAll('.nav__link')
+
+const linkAction = () =>{
+    const navMenu = document.getElementById('nav-menu')
+    navMenu.classList.remove('show-menu')
+}
+navLink.forEach(n => n.addEventListener('click', linkAction))
+
+const scrollHeader = () =>{
+    const header = document.getElementById('header')
+    this.scrollY >= 50 ? header.classList.add('scroll-header') : header.classList.remove('scroll-header')
+}
+window.addEventListener('scroll', scrollHeader)
+
+const StartUpSound = document.querySelector("#startup-sound")
+const StartButton = document.querySelector(".home__button")
+
+StartButton.addEventListener("click", () => {
+    StartUpSound.play()
+})
+
+let TopButton = document.getElementById("top-button")
+
+window.onscroll = function() {ScrollFunction()}
+
+function ScrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        TopButton.style.display = "block"
+    } else {
+        TopButton.style.display = "none"
+    }
+}
+
+function ScrollToTop() {
+    document.body.scrollTop = 0
+    document.documentElement.scrollTop = 0
+}
+
+const sections = document.querySelectorAll('section[id]')
+
+const scrollActive = () =>{
+  	const scrollY = window.pageYOffset
+
+	sections.forEach(current =>{
+		const sectionHeight = current.offsetHeight,
+			  sectionTop = current.offsetTop - 58,
+			  sectionId = current.getAttribute('id'),
+			  sectionsClass = document.querySelector('.nav__menu a[href*=' + sectionId + ']')
+
+		if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight){
+			sectionsClass.classList.add('active-link')
+		}else{
+			sectionsClass.classList.remove('active-link')
+		}                                                    
+	})
+}
+window.addEventListener('scroll', scrollActive)
