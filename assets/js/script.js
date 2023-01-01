ef3228a7d744d71c19e0f2fe4816caf5572d597f
Koshyar "Zhiyar" Rashidzadeh <89919974+Koshyar-r@users.noreply.github.com>
2023-01-01 10:44:22 -0800

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -46,6 +46,11 @@ function ScrollFunction() {
     } else {
         TopButton.style.display = "none"
     }
+	
+    let winScroll = document.body.scrollTop || document.documentElement.scrollTop
+    let height = document.documentElement.scrollHeight - document.documentElement.clientHeight
+    let scrolled = (winScroll / height) * 100
+    document.getElementById("progress-bar").style.width = scrolled + "%"
 }
 
 function ScrollToTop() {
